9fddbf4ec37da9a84da06d8c4786c737a8d97534
Falko Schmidt <scf@panter.ch>
2018-12-31 16:37:08 +0100

    Provide base URI.

--- src/main/java/de/wasenweg/comix/ComicScanner.java
+++ src/main/java/de/wasenweg/comix/ComicScanner.java
@@ -1,8 +1,26 @@
 package de.wasenweg.comix;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ComicScanner {
 
-	public static String run() {
-        return "Scanning for comics!";
+	public static String run() throws IOException {
+        return "Found comics: " + collectFiles("sample");
+	}
+	
+	private static List<String> collectFiles(String path) throws IOException {
+		Path root = Paths.get(path);
+	    try (Stream<Path> files = Files.walk(root)) {
+	        return files.filter(p -> Files.isRegularFile(p))
+	                    .filter(p -> p.getFileName().toString().endsWith(".cbz"))
+	                    .map(p -> root.relativize(p).toString())
+	                    .collect(Collectors.toList());
+	    }
 	}
 }
--- src/main/java/de/wasenweg/comix/ComicScannerController.java
+++ src/main/java/de/wasenweg/comix/ComicScannerController.java
@@ -1,13 +1,17 @@
 package de.wasenweg.comix;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("${spring.data.rest.base-path}")
 @RestController
 public class ComicScannerController {
 
     @RequestMapping("/scan")
-    public String scan() {
+    public String scan() throws IOException {
     	return ComicScanner.run();
     }
 }
