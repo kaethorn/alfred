5416fd4e9a3b4148b743c55b3e02a3ef78e71d9d
Falko Schmidt <scf@panter.ch>
2019-01-03 22:22:09 +0100

    Handle exceptions in private utility method.

--- api/src/main/java/de/wasenweg/comix/ComicScanner.java
+++ api/src/main/java/de/wasenweg/comix/ComicScanner.java
@@ -17,7 +17,7 @@ public class ComicScanner {
 
 	private final static String COMICS_PATH = "../sample";
 	
-	private static Comic readMetadata(Path path) throws FileNotFoundException, IOException {
+	private static Comic readMetadata(Path path) {
 		Comic comic = new Comic(path.toAbsolutePath().toString(), "", "", (short) 0, (short) 0, (short) 0, "");
 		try (FileInputStream fis = new FileInputStream(path.toString());
              BufferedInputStream bis = new BufferedInputStream(fis);
@@ -30,7 +30,9 @@ public class ComicScanner {
                 files += ze.getName() + "(" + ze.getSize() + ");";
             }
             comic.setSummary(files);
-        }
+        } catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
 		return comic;
 	}
 	
@@ -39,16 +41,8 @@ public class ComicScanner {
 	    try (Stream<Path> files = Files.walk(root)) {
 	        return files.filter(path -> Files.isRegularFile(path))
 	                    .filter(path -> path.getFileName().toString().endsWith(".cbz"))
-	                    .map(path -> {
-							try {
-								return readMetadata(path);
-							} catch (FileNotFoundException e) {
-								return new Comic();
-							} catch (IOException e) {
-								return new Comic();
-							}
-						})
-	                    .filter(path -> path.getTitle() != null)
+	                    .map(path -> readMetadata(path))
+	                    .filter(path -> !path.getTitle().isEmpty())
 	                    .collect(Collectors.toList());
 	    }
 	}
