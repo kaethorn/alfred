8ade11eb26da51b67a3d5d86319ca63f85eb5c91
Falko Schmidt <scf@panter.ch>
2019-01-04 22:49:51 +0100

    Apply material design.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,10 +1,21 @@
+// Third party
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { InjectableRxStompConfig, RxStompService, rxStompServiceFactory } from '@stomp/ng2-stompjs';
 
-import { RxStompConfig } from './rx-stomp.config';
+// Material design
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
+// Own
+import { RxStompConfig } from './rx-stomp.config';
 import { AppComponent } from './app.component';
 import { ComicsComponent } from './comics/comics.component';
 
@@ -15,7 +26,15 @@ import { ComicsComponent } from './comics/comics.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatChipsModule,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [{
     provide: InjectableRxStompConfig,
