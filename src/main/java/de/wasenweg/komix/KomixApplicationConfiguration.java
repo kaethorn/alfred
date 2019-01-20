afbf83094650f8a283ab3e8fe649082d94348ead
Falko Schmidt <scf@panter.ch>
2019-01-20 21:40:57 +0100

    Fix style violations.

--- src/main/java/de/wasenweg/komix/KomixApplicationConfiguration.java
+++ src/main/java/de/wasenweg/komix/KomixApplicationConfiguration.java
@@ -1,15 +1,14 @@
 package de.wasenweg.komix;
 
-import java.util.Collections;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Configuration
 public class KomixApplicationConfiguration {
@@ -18,7 +17,10 @@ public class KomixApplicationConfiguration {
     ErrorViewResolver redirectViewUrls() {
         return new ErrorViewResolver() {
             @Override
-            public ModelAndView resolveErrorView(final HttpServletRequest request, final HttpStatus status, final Map<String, Object> model) {
+            public ModelAndView resolveErrorView(
+                    final HttpServletRequest request,
+                    final HttpStatus status,
+                    final Map<String, Object> model) {
                 return status == HttpStatus.NOT_FOUND
                         ? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
                         : null;
