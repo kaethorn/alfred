fb4fdb3b6e06899617089f8669b0741ed3a4d10b
Falko Schmidt <scf@panter.ch>
2019-01-20 21:14:14 +0100

    Fix ViewController.

--- src/main/java/de/wasenweg/komix/ViewController.java
+++ src/main/java/de/wasenweg/komix/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-    @RequestMapping("/!api")
+    @RequestMapping({ "/comics/**",  "/read/**", "/preferences/**", "/read-full-screen/**" })
     public String redirect() {
         return "forward:/index.html";
     }
