237965526abee2edf6ea20bfe6645254b4e07247
Falko Schmidt <scf@panter.ch>
2019-01-21 21:37:48 +0100

    Remove unused import.

--- src/main/java/de/wasenweg/komix/scanner/ScannerController.java
+++ src/main/java/de/wasenweg/komix/scanner/ScannerController.java
@@ -7,7 +7,6 @@ import de.wasenweg.komix.preferences.PreferenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
