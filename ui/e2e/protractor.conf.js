668c09d8e50d5980e10b1e3667531f1dd56da21d
Falko Schmidt <scf@panter.ch>
2019-02-03 15:07:00 +0100

    Run protractor tests with Gradle.

--- ui/e2e/protractor.conf.js
+++ ui/e2e/protractor.conf.js
@@ -5,24 +5,24 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
+  specs            : [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome'
   },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
+  directConnect  : true,
+  baseUrl        : 'http://localhost:4200/',
+  framework      : 'jasmine',
   jasmineNodeOpts: {
-    showColors: true,
+    showColors            : true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print                 : function () {}
   },
-  onPrepare() {
+  onPrepare () {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true }}));
   }
 };
--- ui/e2e/src/app.e2e-spec.ts
+++ ui/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to KomiX!');
+    expect(page.getTitleText()).toContain('Welcome to KomiX');
   });
 });
