748b56a24526f739bb3223dff045b9712e072556
Falko Schmidt <scf@panter.ch>
2019-02-08 14:40:29 +0100

    Change header logo.

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KomiX'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('KomiX');
-  });
-
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('mat-toolbar').textContent).toContain('Welcome to KomiX');
+    expect(compiled.querySelector('mat-toolbar').textContent).toContain('KomX');
   });
 });
