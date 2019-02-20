ebe7d89fb2e2534fc1543123945907df66319b69
Falko Schmidt <scf@panter.ch>
2019-02-20 23:42:55 +0100

    Move to WebFlux service.

--- src/main/java/de/wasenweg/komix/scanner/ScannerController.java
+++ src/main/java/de/wasenweg/komix/scanner/ScannerController.java
@@ -1,47 +1,22 @@
 package de.wasenweg.komix.scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api")
 public class ScannerController {
 
-    private final Semaphore running = new Semaphore(1);
-
-    private final List<SseEmitter> emitters = new ArrayList<>();
-
     @Autowired
     private ScannerService scannerService;
 
     @GetMapping("/scan-progress")
-    SseEmitter streamScanProgress() {
-        final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-        this.emitters.add(emitter);
-
-        emitter.onCompletion(() -> this.emitters.remove(emitter));
-        emitter.onTimeout(() -> {
-            emitter.complete();
-            this.emitters.remove(emitter);
-        });
-
-        if (running.tryAcquire()) {
-            Executors.newSingleThreadExecutor().execute(() -> {
-                scannerService.scanComics(emitters);
-                running.release();
-            });
-        } else {
-            emitter.complete();
-        }
-
-        return emitter;
+    Flux<ServerSentEvent<String>> streamScanProgress() {
+        return scannerService.scanComics();
     }
 }
