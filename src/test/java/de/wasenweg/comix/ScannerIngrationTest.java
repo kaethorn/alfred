6fa4f14773483150a9ca8b7a7e78941f425f19f7
Falko Schmidt <scf@panter.ch>
2019-02-25 08:25:09 +0100

    Use StepVerifier to validate SSE.

--- src/test/java/de/wasenweg/comix/ScannerIngrationTest.java
+++ src/test/java/de/wasenweg/comix/ScannerIngrationTest.java
@@ -1,11 +1,9 @@
 package de.wasenweg.comix;
 
 import de.wasenweg.komix.KomixApplication;
-import de.wasenweg.komix.comics.Comic;
 import de.wasenweg.komix.comics.ComicRepository;
 import de.wasenweg.komix.preferences.Preference;
 import de.wasenweg.komix.preferences.PreferenceRepository;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,18 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import java.time.Duration;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KomixApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -60,35 +54,17 @@ public class ScannerIngrationTest {
         final WebClient client = WebClient
                 .create("http://localhost:" + port + "/api");
 
-        final ParameterizedTypeReference<ServerSentEvent<String>> type =
-                new ParameterizedTypeReference<ServerSentEvent<String>>() { };
-
-        final List<ServerSentEvent<String>> events = new ArrayList<ServerSentEvent<String>>();
-
-        final CompletableFuture<String> future = new CompletableFuture<>();
-        client.get()
+        final Flux<String> result = client.get()
             .uri("/scan-progress")
+            .accept(TEXT_EVENT_STREAM)
             .retrieve()
-            .bodyToFlux(type)
-            .subscribe(
-                content -> {
-                    if (content.event().equals("done")) {
-                        future.complete("Complete");
-                    }
-                    events.add(content);
-                }
-        );
-        assertEquals("Complete", future.get());
-        final List<Comic> comics = StreamSupport
-                .stream(this.comicRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
-        assertThat(comics.size()).isEqualTo(1);
+            .bodyToFlux(new ParameterizedTypeReference<String>() { });
 
-        assertThat(events.get(0).event()).isEqualTo("total");
-        assertThat(events.get(0).data()).isEqualTo("1");
-        assertThat(events.get(1).event()).isEqualTo("current-file");
-        assertThat(events.get(1).data()).isEqualTo("src/test/resources/fixtures/simple/Batman 402 (1940).cbz");
-        assertThat(events.get(2).event()).isEqualTo("done");
-        assertThat(events.get(2).data()).isEqualTo("");
+        StepVerifier.create(result)
+            .expectNext("1")
+            .expectNext("src/test/resources/fixtures/simple/Batman 402 (1940).cbz")
+            .expectNext("")
+            .thenCancel()
+            .verify(Duration.ofSeconds(2L));
     }
 }
