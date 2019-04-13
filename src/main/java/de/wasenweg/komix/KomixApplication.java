b2c215c3fb10fdf4d9039bb233349bdef2469d86
Falko Schmidt <scf@panter.ch>
2019-04-13 12:14:28 +0000

    Refactor security config.

--- src/main/java/de/wasenweg/komix/KomixApplication.java
+++ src/main/java/de/wasenweg/komix/KomixApplication.java
@@ -2,29 +2,11 @@ package de.wasenweg.komix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
-@EnableOAuth2Sso
-public class KomixApplication extends WebSecurityConfigurerAdapter {
+public class KomixApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(KomixApplication.class, args);
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-      http
-        .antMatcher("/**")
-          .authorizeRequests()
-            .antMatchers("/", "/login**", "/error**")
-            .permitAll()
-            .anyRequest().authenticated()
-          .and()
-        .logout().logoutUrl("/api/logout").logoutSuccessUrl("/").permitAll()
-        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
---
+++ src/main/java/de/wasenweg/komix/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package de.wasenweg.komix;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+      http
+        .antMatcher("/**")
+          .authorizeRequests()
+            .antMatchers("/", "/login**", "/error**")
+            .permitAll()
+            .anyRequest().authenticated()
+          .and()
+        .logout().logoutUrl("/api/logout").logoutSuccessUrl("/").permitAll()
+        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
