b945796dd96a60fc3345d283a6d10c6e051a70bf
Falko Schmidt <scf@panter.ch>
2019-04-19 20:27:29 +0200

    Show read count in library view.

--- src/main/java/de/wasenweg/alfred/comics/ComicQueryRepositoryImpl.java
+++ src/main/java/de/wasenweg/alfred/comics/ComicQueryRepositoryImpl.java
@@ -1,31 +1,26 @@
 package de.wasenweg.alfred.comics;
 
-import com.mongodb.BasicDBObject;
+import de.wasenweg.alfred.progress.ProgressHelper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.ComparisonOperators;
 import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
 import org.springframework.data.mongodb.core.query.Criteria;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.limit;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.lookup;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.replaceRoot;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
 import static org.springframework.data.mongodb.core.aggregation.ArrayOperators.ArrayElemAt.arrayOf;
 import static org.springframework.data.mongodb.core.aggregation.ArrayOperators.Filter.filter;
-import static org.springframework.data.mongodb.core.aggregation.ObjectOperators.MergeObjects.merge;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 public class ComicQueryRepositoryImpl implements ComicQueryRepository {
@@ -33,35 +28,10 @@ public class ComicQueryRepositoryImpl implements ComicQueryRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    // Merge progress flags for the current user
-    private List<AggregationOperation> mergeProgress(final String userId) {
-        return Stream.of(
-            lookup("progress", "_id", "comicId", "progress"),
-            replaceRoot().withValueOf(
-                merge(Aggregation.ROOT)
-                .mergeWithValuesOf(
-                    arrayOf(
-                        filter("progress").as("item").by(ComparisonOperators.Eq.valueOf("item.userId").equalToValue(userId))
-                    ).elementAt(0))
-                // Restore comic _id previously overwritten with progress _id.
-                .mergeWith(new BasicDBObject("_id", Aggregation.ROOT + "._id"))),
-            project().andExclude("progress", "comicId", "userId")
-        ).collect(Collectors.toList());
-    }
-
-    // Syntactic sugar
-    private Aggregation aggregateWithProgress(final String userId, final AggregationOperation... operations) {
-        return Aggregation.newAggregation(
-                Stream.concat(
-                        mergeProgress(userId).stream(),
-                        Stream.of(operations))
-                .collect(Collectors.toList()));
-    }
-
     // Lists issues for volumes that are in progress, aka bookmarks.
     @Override
     public List<Comic> findAllLastReadPerVolume(final String userId) {
-        return mongoTemplate.aggregate(aggregateWithProgress(userId,
+        return mongoTemplate.aggregate(ProgressHelper.aggregateWithProgress(userId,
             // Collect volumes with aggregated read stats and a list of issues
             sort(Sort.Direction.ASC, "position"),
             group("publisher", "series", "volume")
@@ -111,7 +81,7 @@ public class ComicQueryRepositoryImpl implements ComicQueryRepository {
             final String series,
             final String volume) {
 
-        return Optional.ofNullable(mongoTemplate.aggregate(aggregateWithProgress(userId,
+        return Optional.ofNullable(mongoTemplate.aggregate(ProgressHelper.aggregateWithProgress(userId,
             match(where("publisher").is(publisher).and("series").is(series).and("volume").is(volume)),
 
             // If all comics are read, return the first, otherwise the first unread
@@ -128,7 +98,7 @@ public class ComicQueryRepositoryImpl implements ComicQueryRepository {
             final String publisher,
             final String series,
             final String volume) {
-        return mongoTemplate.aggregate(aggregateWithProgress(userId,
+        return mongoTemplate.aggregate(ProgressHelper.aggregateWithProgress(userId,
             match(where("publisher").is(publisher).and("series").is(series).and("volume").is(volume)),
 
             sort(Sort.Direction.ASC, "position")
--- src/main/java/de/wasenweg/alfred/publisher/PublisherController.java
+++ src/main/java/de/wasenweg/alfred/publisher/PublisherController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -19,8 +20,8 @@ public class PublisherController {
     private PublisherQueryRepositoryImpl repository;
 
     @GetMapping
-    public Resources<Publisher> findAll() {
-        final List<Publisher> publishers = this.repository.findAll();
+    public Resources<Publisher> findAll(final Principal principal) {
+        final List<Publisher> publishers = this.repository.findAll(principal.getName());
         final Link link = linkTo(PublisherController.class).withSelfRel();
         return new Resources<Publisher>(publishers, link);
     }
