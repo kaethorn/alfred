b21c197055aee911da8a1f9f7e55822730ddad4d
Falko Schmidt <scf@panter.ch>
2019-04-23 21:09:06 +0200

    Make sure menu items are clickable.

--- ui/e2e/src/page.po.ts
+++ ui/e2e/src/page.po.ts
@@ -1,12 +1,11 @@
-import { browser, by, element, ElementFinder, ExpectedConditions } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class Page {
 
   async clickMenuItem(target: ElementFinder, item: string) {
     await target.element(by.cssContainingText('mat-icon', 'more_vert')).click();
 
-    const linkElement = element(by.partialButtonText(item));
-    await browser.wait(ExpectedConditions.elementToBeClickable(linkElement), 200);
-    await linkElement.click();
+    await browser.sleep(500);
+    await element(by.partialButtonText(item)).click();
   }
 }
