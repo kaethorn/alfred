dd4a3f7e45058b34f02d1c3cbe5c177be047a548
Falko Schmidt <scf@panter.ch>
2019-06-05 14:35:00 +0200

    Provide login overlay.

--- ui/src/app/user.service.ts
+++ ui/src/app/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { User } from './user';
@@ -9,11 +9,34 @@ import { User } from './user';
 export class UserService {
 
   private user: Observable<User>;
-
+  private auth2;
   private updateUser: Function;
 
   constructor (
+    private ngZone: NgZone,
   ) {
+    (window as any).gapi.load('auth2', () => {
+      this.ngZone.run(() => {
+        this.auth2 = (window as any).gapi.auth2.init({
+          client_id: '401455891931-28afa7q3453j1fsdfnlen5tf46oqeadr.apps.googleusercontent.com'
+        });
+        this.auth2.attachClickHandler('signin-button', {}, (googleUser) => {
+          const user = googleUser.getBasicProfile();
+          this.set({
+            email:  user.getEmail(),
+            name: user.getName(),
+            picture: user.getImageUrl()
+          });
+        }, (errorData) => {
+          console.log(`Login failure: ${ errorData }`);
+        });
+
+        if (this.auth2.isSignedIn.get() === true) {
+          this.auth2.signIn();
+        }
+      });
+    });
+
     this.user = new Observable<User>((observer) => {
       this.updateUser = (user) => {
         observer.next(user);
@@ -27,5 +50,17 @@ export class UserService {
 
   get (): Observable<User> {
     return this.user;
+  }
+
+  logout (): Observable<void> {
+    return new Observable((observer) => {
+      (window as any).gapi.auth2.getAuthInstance().signOut().then(() => {
+        this.ngZone.run(() => {
+          this.user = null;
+          observer.next();
+          observer.complete();
+        });
+      });
+    });
   }
 }
