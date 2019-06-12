bdee921c7f22538e84eadda013acba4e31581117
Falko Schmidt <scf@panter.ch>
2019-06-12 13:12:25 +0200

    Use fallback mock user.

--- ui/src/app/user.service.ts
+++ ui/src/app/user.service.ts
@@ -32,27 +32,39 @@ export class UserService {
   }
 
   setupGoogleSignIn () {
-    gapi.load('auth2', () => {
-      this.ngZone.run(() => {
-        this.auth2 = gapi.auth2.init({
-          client_id: '401455891931-28afa7q3453j1fsdfnlen5tf46oqeadr.apps.googleusercontent.com'
-        });
-        this.auth2.attachClickHandler('signin-button', {}, (googleUser: gapi.auth2.GoogleUser) => {
-          const token = googleUser.getAuthResponse().id_token;
-          this.http.post<User>(`api/user/sign-in/${ token }`, null).subscribe((user: User) => {
-            this.user.next(user);
-            localStorage.setItem('token', user.token);
-            localStorage.setItem('user', JSON.stringify(user));
+    if (typeof gapi === 'object') {
+      gapi.load('auth2', () => {
+        this.ngZone.run(() => {
+          this.auth2 = gapi.auth2.init({
+            client_id: '401455891931-28afa7q3453j1fsdfnlen5tf46oqeadr.apps.googleusercontent.com'
           });
-        }, (errorData) => {
-          console.log(`Login failure: ${ JSON.stringify(errorData, null, 2) }`);
-        });
+          this.auth2.attachClickHandler('signin-button', {}, (googleUser: gapi.auth2.GoogleUser) => {
+            const token = googleUser.getAuthResponse().id_token;
+            this.http.post<User>(`api/user/sign-in/${ token }`, null).subscribe((user: User) => {
+              this.user.next(user);
+              localStorage.setItem('token', user.token);
+              localStorage.setItem('user', JSON.stringify(user));
+            });
+          }, (errorData) => {
+            console.log(`Login failure: ${ JSON.stringify(errorData, null, 2) }`);
+          });
 
-        if (this.auth2.isSignedIn.get() === true) {
-          this.auth2.signIn();
-        }
+          if (this.auth2.isSignedIn.get() === true) {
+            this.auth2.signIn();
+          }
+        });
       });
-    });
+    } else {
+      const mockUser: User = {
+        email: 'b.wayne@waynecorp.com',
+        name: 'B.Wayne',
+        picture: 'https://img.icons8.com/office/80/000000/batman-old.png',
+        token: 'mock-123'
+      };
+      this.http.get(`api/user/verify/${ mockUser.token }`).subscribe(() => {
+        this.user.next(mockUser);
+      });
+    }
   }
 
   logout () {
