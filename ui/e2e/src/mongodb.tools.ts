b1164542485cc4069bc59e4a0cd493931c271211
Falko Schmidt <scf@panter.ch>
2019-07-16 20:38:12 +0200

    Reuse mongodb connections during e2e tests.

--- ui/e2e/src/mongodb.tools.ts
+++ ui/e2e/src/mongodb.tools.ts
@@ -2,14 +2,20 @@ import * as mongoose from 'mongoose';
 
 export class MongoDBTools {
 
+  private static connection: Promise<any> = new Promise((resolve, reject) => {
+    mongoose.connect('mongodb://localhost/alfred');
+    mongoose.connection.on('error', error => {
+      reject(error);
+    });
+    mongoose.connection.once('open', () => {
+      resolve();
+    });
+  });
+
   // Drop DB and set it up it for E2E tests.
   static prepare (): Promise<any> {
     return new Promise((resolve, reject) => {
-      mongoose.connect('mongodb://localhost/alfred');
-      mongoose.connection.on('error', error => {
-        reject(error);
-      });
-      mongoose.connection.once('open', () => {
+      this.connection.then(() => {
         mongoose.connection.db.dropDatabase().then(() => {
           mongoose.connection.db.createCollection('setting').then(collection => {
             collection.insertOne({
