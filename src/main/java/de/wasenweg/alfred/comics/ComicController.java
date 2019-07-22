a154865c57923486b088cc930a38bfdf8a071898
Falko Schmidt <scf@panter.ch>
2019-07-22 17:08:55 +0200

    Establish base controller providing HATEOAS utility methods.

--- src/main/java/de/wasenweg/alfred/comics/ComicController.java
+++ src/main/java/de/wasenweg/alfred/comics/ComicController.java
@@ -2,11 +2,10 @@ package de.wasenweg.alfred.comics;
 
 import de.wasenweg.alfred.progress.ProgressService;
 import de.wasenweg.alfred.scanner.ScannerService;
+import de.wasenweg.alfred.util.BaseController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,15 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 import java.security.Principal;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RequestMapping(value = "/api/comics", produces = { "application/hal+json" })
 @RestController
-public class ComicController {
+public class ComicController extends BaseController<Comic> {
 
   @Autowired
   private ProgressService progressService;
@@ -96,27 +91,5 @@ public class ComicController {
   @GetMapping("/bundle")
   public void bundle() {
     this.scannerService.associateVolumes();
-  }
-
-  private Resources<Resource<Comic>> addCollectionLink(final List<Comic> comics) {
-    return new Resources<Resource<Comic>>(
-        comics.stream()
-        .map(comic -> {
-          return this.addLink(comic);
-        }).collect(Collectors.toList()),
-        linkTo(ComicController.class).withSelfRel());
-  }
-
-  private Resource<Comic> addLink(final Optional<Comic> comic) {
-    if (comic.isPresent()) {
-      return this.addLink(comic.get());
-    } else {
-      throw new ResourceNotFoundException();
-    }
-  }
-
-  private Resource<Comic> addLink(final Comic comic) {
-    final Link link = linkTo(ComicController.class).slash(comic.getId()).withSelfRel();
-    return new Resource<Comic>(comic, link);
   }
 }
