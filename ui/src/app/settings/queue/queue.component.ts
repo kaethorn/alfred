1cf63138b74931b694817d162faf7d54a97bdb64
Falko Schmidt <scf@panter.ch>
2020-02-13 12:34:56 +0100

    Remove queue item ignore button.

--- ui/src/app/settings/queue/queue.component.ts
+++ ui/src/app/settings/queue/queue.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
 
 import { Comic } from '../../comic';
 import { ComicsService } from '../../comics.service';
@@ -16,7 +15,6 @@ export class QueueComponent {
 
   constructor (
     private comicsService: ComicsService,
-    private toastController: ToastController,
   ) { }
 
   ionViewWillEnter () {
@@ -28,23 +26,5 @@ export class QueueComponent {
       .subscribe((data: Comic[]) => {
         this.comics = data;
       });
-  }
-
-  ignore (comic: Comic): void {
-    delete comic.errors;
-    this.comicsService.update(comic).subscribe(() => {
-      this.showToast('Comic errors ignored.');
-      this.list();
-    }, () => {
-      this.showToast('Error while ignoring comic errors.');
-    });
-  }
-
-  private async showToast (message: string, duration: number = 2000) {
-    const toast = await this.toastController.create({
-      message,
-      duration
-    });
-    toast.present();
   }
 }
