4cff623743bf22bb8f7e23157926b1c888a9aa1d
Falko Schmidt <scf@panter.ch>
2020-02-19 23:27:20 +0100

    Upgrade to latest angular & ionic version.

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,16 +3,38 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'library', pathMatch: 'full' },
-  { path: 'library', loadChildren: './library/library.module#LibraryPageModule', canActivate: [AuthGuard] },
-  { path: 'issues/:publisher/:series/:volume', loadChildren: './issues/issues.module#IssuesPageModule', canActivate: [AuthGuard] },
-  { path: 'read/:id', loadChildren: './reader/reader.module#ReaderPageModule', canActivate: [AuthGuard] },
-  { path: 'bookmarks', loadChildren: './bookmarks/bookmarks.module#BookmarksPageModule', canActivate: [AuthGuard] },
-  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule', canActivate: [AuthGuard] },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'edit', loadChildren: './edit/edit.module#EditPageModule', canActivate: [AuthGuard] },
-];
+const routes: Routes = [{
+    path: '',
+    redirectTo: 'library',
+    pathMatch: 'full'
+  }, {
+    path: 'library',
+    loadChildren: () => import('./library/library.module').then(m => m.LibraryPageModule),
+    canActivate: [AuthGuard]
+  }, {
+    path: 'issues/:publisher/:series/:volume',
+    loadChildren: () => import('./issues/issues.module').then(m => m.IssuesPageModule),
+    canActivate: [AuthGuard]
+  }, {
+    path: 'read/:id',
+    loadChildren: () => import('./reader/reader.module').then(m => m.ReaderPageModule),
+    canActivate: [AuthGuard]
+  }, {
+    path: 'bookmarks',
+    loadChildren: () => import('./bookmarks/bookmarks.module').then(m => m.BookmarksPageModule),
+    canActivate: [AuthGuard]
+  }, {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule),
+    canActivate: [AuthGuard]
+  }, {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  }, {
+    path: 'edit',
+    loadChildren: () => import('./edit/edit.module').then(m => m.EditPageModule),
+    canActivate: [AuthGuard]
+  }];
 
 @NgModule({
   imports: [
--- ui/src/polyfills.ts
+++ ui/src/polyfills.ts
@@ -55,7 +55,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import './zone-flags.ts';
+import './zone-flags';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
