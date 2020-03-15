1f11e95f0680d12c12c69218df640642f4219d54
Falko Schmidt <scf@panter.ch>
2020-03-15 17:07:32 +0100

    Test bookmark actions component.

--- ui/src/app/bookmarks/bookmark-actions/bookmark-actions.component.spec.ts
+++ ui/src/app/bookmarks/bookmark-actions/bookmark-actions.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { NavParams } from '@ionic/angular';
+import { NavParams, PopoverController } from '@ionic/angular';
 
 import { ComicFixtures } from '../../../testing/comic.fixtures';
+import { PopoverControllerMocks } from '../../../testing/popover.controller.mocks';
 import { BookmarksPageModule } from '../bookmarks.module';
 
 import { BookmarkActionsComponent } from './bookmark-actions.component';
@@ -10,11 +12,16 @@ import { BookmarkActionsComponent } from './bookmark-actions.component';
 let component: BookmarkActionsComponent;
 let fixture: ComponentFixture<BookmarkActionsComponent>;
 let navParams: NavParams;
+let router: jasmine.SpyObj<Router>;
+let popoverController: jasmine.SpyObj<PopoverController>;
 
 describe('BookmarkActionsComponent', () => {
 
   beforeEach(() => {
+    router = jasmine.createSpyObj('Router', ['navigate']);
+    popoverController = PopoverControllerMocks.popoverController;
     navParams = new NavParams({ comic: ComicFixtures.comic });
+
     TestBed.configureTestingModule({
       imports: [
         BookmarksPageModule,
@@ -22,6 +29,10 @@ describe('BookmarkActionsComponent', () => {
       ],
       providers: [{
         provide: NavParams, useValue: navParams
+      }, {
+        provide: Router, useValue: router
+      }, {
+        provide: PopoverController, useValue: popoverController
       }]
     });
     fixture = TestBed.createComponent(BookmarkActionsComponent);
@@ -31,5 +42,34 @@ describe('BookmarkActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#gotToVolume', () => {
+
+    it('navigates to the issues page and closes the popover', () => {
+      component.goToVolume(ComicFixtures.comic);
+      expect(router.navigate).toHaveBeenCalledWith([
+        '/issues',
+        ComicFixtures.comic.publisher,
+        ComicFixtures.comic.series,
+        ComicFixtures.comic.volume
+      ]);
+      expect(popoverController.dismiss).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('#gotToLibrary', () => {
+
+    it('navigates to the library page and closes the popover', () => {
+      component.goToLibrary(ComicFixtures.comic);
+      expect(router.navigate).toHaveBeenCalledWith([
+        '/library/publishers',
+        ComicFixtures.comic.publisher,
+        'series',
+        ComicFixtures.comic.series,
+        'volumes'
+      ]);
+      expect(popoverController.dismiss).toHaveBeenCalledWith();
+    });
   });
 });
