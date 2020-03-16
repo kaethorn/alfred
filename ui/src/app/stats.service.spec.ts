9d3ffe607a77eaa534e7b9ac439198cee8dd2786
Falko Schmidt <scf@panter.ch>
2020-03-16 17:03:22 +0100

    Test stats service.

---
+++ ui/src/app/stats.service.spec.ts
@@ -0,0 +1,42 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { StatsHttpMocks } from '../testing/stats.http.mocks';
+
+import { StatsService } from './stats.service';
+
+let service: StatsService;
+let httpMock: HttpTestingController;
+
+describe('StatsService', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+    service = TestBed.inject(StatsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+    TestBed.resetTestingModule();
+  });
+
+  describe('#get', () => {
+
+    it('lists all statistics', () => {
+      service.get().subscribe(stats => {
+        expect(stats.issues).toBe(305);
+        expect(stats.publishers).toBe(3);
+        expect(stats.series).toBe(5);
+        expect(stats.volumes).toBe(9);
+        expect(stats.users).toBe(1);
+      });
+
+      const req = httpMock.expectOne('/api/stats');
+      expect(req.request.method).toBe('GET');
+      req.flush(StatsHttpMocks.stats);
+    });
+  });
+});
