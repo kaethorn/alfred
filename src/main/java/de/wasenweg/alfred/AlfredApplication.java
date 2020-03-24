a665ce9f7e54c02e5474205454fa073f2b7a176b
Falko Schmidt <scf@panter.ch>
2020-03-24 19:41:50 +0100

    Refactor error controller to separate class.

--- src/main/java/de/wasenweg/alfred/AlfredApplication.java
+++ src/main/java/de/wasenweg/alfred/AlfredApplication.java
@@ -3,29 +3,13 @@ package de.wasenweg.alfred;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @EnableCaching
 @SpringBootApplication(exclude = EmbeddedMongoAutoConfiguration.class)
-public class AlfredApplication implements ErrorController {
-
-  private static final String ERROR_PATH = "/error";
+public class AlfredApplication {  // NOPMD
 
   public static void main(final String[] args) {
     SpringApplication.run(AlfredApplication.class, args);
-  }
-
-  @RequestMapping(ERROR_PATH)
-  public String error() {
-    return "forward:/index.html";
-  }
-
-  @Override
-  public String getErrorPath() {
-    return ERROR_PATH;
   }
 }
