07675a8ff547a7a35aede44a85b10592990d8d7b
Falko Schmidt <scf@panter.ch>
2021-10-26 08:20:15 +0200

    feat: update dependencies (#110)

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,4 +1,5 @@
-import { version } from '../../package.json';
+import packageJson from '../../package.json';
+const version = packageJson.version;
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
@@ -16,4 +17,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
--- ui/src/test.ts
+++ ui/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
 import 'hammerjs';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -10,8 +10,8 @@ import {
 
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
     <T>(id: string): T;
+    keys(): string[];
   };
 };
 
